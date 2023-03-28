856af1f9118566d8c2b275dac6358a8767fee5da
hsHarshita <123306118+hsHarshita@users.noreply.github.com>
2023-03-28 13:06:09 +0530

    Create var.tf

---
+++ TF_files/var.tf
@@ -0,0 +1,7 @@
+variable "ami" { # We are declaring the variable ami here which we used in main.tf
+  type = string
+}
+
+variable "instance_type" { # We are declaring the variable instance_type here which we used in main.tf
+  type = string
+}
